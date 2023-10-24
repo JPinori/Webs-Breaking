e36a4a9ed38d33e784b5d59142c182de0cdf239c
JPinori <102628226+JPinori@users.noreply.github.com>
2023-10-24 19:39:47 -0300

    Create SkipIntroHBO.js

---
+++ JS/SkipIntroHBO.js
@@ -0,0 +1,5 @@
+setInterval(function(){   
+       if(document.getElementsByClassName('button-primary watch-video--skip-content-button medium hasLabel ltr-1d02up3').length > 0){
+      document.getElementsByClassName('button-primary watch-video--skip-content-button medium hasLabel ltr-1d02up3')[0].click()
+       }
+      }  , 1000)
