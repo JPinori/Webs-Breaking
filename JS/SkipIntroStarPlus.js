bda597530268e9cae0c7415dc6dbd91e0b16dda2
JPinori <102628226+JPinori@users.noreply.github.com>
2023-10-24 19:39:17 -0300

    Create SkipIntroStarPlus.js

---
+++ JS/SkipIntroStarPlus.js
@@ -0,0 +1,5 @@
+setTimeout(function() {setInterval(function(){   
+       if(document.getElementsByClassName('skip__button body-copy').length > 0){
+      document.getElementsByClassName('skip__button body-copy')[0].click()
+       }
+      }  , 1000)}, 4000);
